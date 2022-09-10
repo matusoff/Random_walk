1f51ff7c65429ac323928ea48442c027ea2e2650
matusoff <94809104+matusoff@users.noreply.github.com>
2022-09-10 14:09:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -29,32 +29,3 @@ for _ in range(400):
 #  close screen by mouse click
 my_screen.exitonclick()
 
-
-# draw a spirograph
-from turtle import Turtle, Screen
-import turtle as t
-import random
-
-#to visualize the screen
-my_screen = Screen()
-my_screen.colormode(255)
-#my_screen.bgcolor("black")
-tim = t.Turtle()
-tim.pensize(1)
-tim.speed("fastest")
-tim.hideturtle()
-
-#function to make a random colors
-def random_color():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    random_color = (r,g,b)   
-    return random_color
-
-for _ in range(75):
-    tim.circle(100)
-    tim.left(5)
-    tim.color(random_color())
-
-my_screen.exitonclick()
